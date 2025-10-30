868996ba1c463648827c02be6cd1735a20134a4e
Carriedied <52948394+Carriedied@users.noreply.github.com>
2025-10-30 12:10:38 +0700

    Syntax Edit

--- 4_lab.cpp
+++ 4_lab.cpp
@@ -18,59 +18,59 @@ int main()
     setlocale(LC_ALL, "russian");
 
     cout << "=== Общие задания (std::vector) ===" << endl;
-    vector<int> common_arr = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5 };
+    vector<int> commonArr = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5 };
 
     cout << "Исходный вектор: ";
-    PrintVector(common_arr);
+    PrintVector(commonArr);
 
-    int max_val, min_val;
+    int maxVal, minVal;
 
-    FindMinMax(common_arr, max_val, min_val);
+    FindMinMax(commonArr, maxVal, minVal);
 
-    cout << "Максимальный элемент: " << max_val << endl;
-    cout << "Минимальный элемент: " << min_val << endl;
+    cout << "Максимальный элемент: " << maxVal << endl;
+    cout << "Минимальный элемент: " << minVal << endl;
 
-    SwapMinMax(common_arr);
+    SwapMinMax(commonArr);
 
     cout << "После перестановки max и min: ";
 
-    PrintVector(common_arr);
+    PrintVector(commonArr);
 
     cout << "Количество различных элементов и их частота:" << endl;
-    CountDistinct(common_arr);
+    CountDistinct(commonArr);
 
     cout << "\n=== Индивидуальные задания (без std::vector) ===" << endl;
 
     int n = 6;
-    double* individual_arr = new double[n] {1.0, 0.5, -1.0, -2.0, 3.0, -0.3};
+    double* individualArr = new double[n] {1.0, 0.5, -1.0, -2.0, 3.0, -0.3};
 
     cout << "Исходный массив: ";
 
     for (int i = 0; i < n; ++i) 
     {
-        cout << individual_arr[i] << " ";
+        cout << individualArr[i] << " ";
     }
 
     cout << endl;
 
-    int min_idx = FindMinIndex(individual_arr, n);
-    cout << "Номер (индекс) минимального элемента: " << min_idx << endl;
+    int minIdx = FindMinIndex(individualArr, n);
+    cout << "Номер (индекс) минимального элемента: " << minIdx << endl;
 
-    double sum = SumBetweenNegatives(individual_arr, n);
+    double sum = SumBetweenNegatives(individualArr, n);
     cout << "Сумма элементов между первым и вторым отрицательными: " << sum << endl;
 
-    RearrangeArray(individual_arr, n);
+    RearrangeArray(individualArr, n);
 
     cout << "Преобразованный массив: ";
 
     for (int i = 0; i < n; ++i) 
     {
-        cout << individual_arr[i] << " ";
+        cout << individualArr[i] << " ";
     }
 
     cout << endl;
 
-    delete[] individual_arr;
+    delete[] individualArr;
 
     return 0;
 }
@@ -85,24 +85,24 @@ void PrintVector(const vector<int>& vec)
     cout << endl;
 }
 
-void FindMinMax(const vector<int>& arr, int& max_val, int& min_val) 
+void FindMinMax(const vector<int>& arr, int& maxVal, int& minVal) 
 {
     if (arr.empty()) 
     {
-        max_val = min_val = 0;
+        maxVal = minVal = 0;
 
         return;
     }
 
-    max_val = min_val = arr[0];
+    maxVal = minVal = arr[0];
 
     for (int num : arr) 
     {
-        if (num > max_val) 
-            max_val = num;
+        if (num > maxVal) 
+            maxVal = num;
 
-        if (num < min_val) 
-            min_val = num;
+        if (num < minVal) 
+            minVal = num;
     }
 }
 
@@ -111,37 +111,37 @@ void SwapMinMax(vector<int>& arr)
     if (arr.empty()) 
         return;
 
-    int max_val, min_val;
+    int maxVal, minVal;
 
-    FindMinMax(arr, max_val, min_val);
+    FindMinMax(arr, maxVal, minVal);
 
-    int max_idx = -1, min_idx = -1;
+    int maxIdx = -1, minIdx = -1;
 
     for (size_t i = 0; i < arr.size(); ++i) 
     {
-        if (arr[i] == max_val && max_idx == -1) 
-            max_idx = i;
+        if (arr[i] == maxVal && maxIdx == -1) 
+            maxIdx = i;
 
-        if (arr[i] == min_val)
-            min_idx = i;
+        if (arr[i] == minVal)
+            minIdx = i;
     }
 
-    if (max_idx != -1 && min_idx != -1) 
+    if (maxIdx != -1 && minIdx != -1) 
     {
-        swap(arr[max_idx], arr[min_idx]);
+        swap(arr[maxIdx], arr[minIdx]);
     }
 }
 
 void CountDistinct(const vector<int>& arr) 
 {
-    map<int, int> freq_map;
+    map<int, int> freqMap;
 
     for (int num : arr) 
     {
-        freq_map[num]++;
+        freqMap[num]++;
     }
 
-    for (const auto& pair : freq_map) 
+    for (const auto& pair : freqMap) 
     {
         cout << "Значение: " << pair.first << ", Частота: " << pair.second << endl;
     }
@@ -152,48 +152,48 @@ int FindMinIndex(const double* arr, int size)
     if (size <= 0) 
         return -1;
 
-    int min_idx = 0;
+    int minIdx = 0;
 
     for (int i = 1; i < size; ++i) 
     {
-        if (arr[i] < arr[min_idx]) 
+        if (arr[i] < arr[minIdx]) 
         {
-            min_idx = i;
+            minIdx = i;
         }
     }
 
-    return min_idx;
+    return minIdx;
 }
 
 double SumBetweenNegatives(const double* arr, int size) 
 {
-    int first_neg_idx = -1;
-    int second_neg_idx = -1;
+    int firstNegIdx = -1;
+    int secondNegIdx = -1;
 
     for (int i = 0; i < size; ++i) 
     {
         if (arr[i] < 0) 
         {
-            if (first_neg_idx == -1) 
+            if (firstNegIdx == -1) 
             {
-                first_neg_idx = i;
+                firstNegIdx = i;
             }
-            else if (second_neg_idx == -1) 
+            else if (secondNegIdx == -1) 
             {
-                second_neg_idx = i;
+                secondNegIdx = i;
                 break;
             }
         }
     }
 
-    if (first_neg_idx == -1 || second_neg_idx == -1 || second_neg_idx - first_neg_idx <= 1) 
+    if (firstNegIdx == -1 || secondNegIdx == -1 || secondNegIdx - firstNegIdx <= 1) 
     {
         return 0.0;
     }
 
     double sum = 0.0;
 
-    for (int i = first_neg_idx + 1; i < second_neg_idx; ++i) 
+    for (int i = firstNegIdx + 1; i < secondNegIdx; ++i) 
     {
         sum += arr[i];
     }
@@ -204,13 +204,13 @@ double SumBetweenNegatives(const double* arr, int size)
 void RearrangeArray(double*& arr, int& size)
 {
     double* temp = new double[size];
-    int write_idx = 0;
+    int writeIdx = 0;
 
     for (int i = 0; i < size; ++i)
     {
         if (abs(arr[i]) <= 1.0)
         {
-            temp[write_idx++] = arr[i];
+            temp[writeIdx++] = arr[i];
         }
     }
 
@@ -218,7 +218,7 @@ void RearrangeArray(double*& arr, int& size)
     {
         if (abs(arr[i]) > 1.0)
         {
-            temp[write_idx++] = arr[i];
+            temp[writeIdx++] = arr[i];
         }
     }
 
